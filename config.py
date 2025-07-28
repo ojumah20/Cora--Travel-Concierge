25600c137617a09e4e1c73e6f99a70e4d70c4527
ojumah20 <onyekachukwuojumah20@gmail.com>
2025-07-28 21:56:04 +0100

    new

--- config.py
+++
@@ -1,4 +0,0 @@
-QLOO_API_KEY = "BpZLlHGs5pUUZFjjizu-a-E5mqfqx7eeKzmzRgcYaqo"
-GROQ_API_KEY = "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"
-GROQ_MODEL = "deepseek-r1-distill-llama-70b"
-PEXELS_API_KEY = "p2CEObXcSyPtWuO4Sjtfl56OMsjKG6jVqOvIhwG9eybNJNENKOnPUeEc"
--- qloo.py
+++ qloo.py
@@ -1,6 +1,7 @@
 import requests
-from config import QLOO_API_KEY
-
+#from config import QLOO_API_KEY
+import streamlit as st
+QLOO_API_KEY = st.secrets["QLOO_API_KEY"]
 # --- Qloo Recommendation Function ---
 def fetch_qloo_recommendations(entity_type, names):
     url = f"https://hackathon.api.qloo.com/culture/v1/{entity_type}/recommendations"
