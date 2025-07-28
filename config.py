c5aaaf89c4fd6ebbcd9fc241f0fa326e93c9e16a
ojumah20 <onyekachukwuojumah20@gmail.com>
2025-07-28 19:58:04 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-QLOO_API_KEY = "BpZLlHGs5pUUZFjjizu-a-E5mqfqx7eeKzmzRgcYaqo"
-GROQ_API_KEY = "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"
-GROQ_MODEL = "deepseek-r1-distill-llama-70b"
-PEXELS_API_KEY = "p2CEObXcSyPtWuO4Sjtfl56OMsjKG6jVqOvIhwG9eybNJNENKOnPUeEc"
+QLOO_API_KEY = ""
+GROQ_API_KEY = ""
+GROQ_MODEL = ""
+PEXELS_API_KEY = ""
