30211520bfba54313bb76491fc8ea577d4462eba
ojumah20 <onyekachukwuojumah20@gmail.com>
2025-07-28 21:27:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-QLOO_API_KEY = "" #update with your key
-GROQ_API_KEY = "" #update with your key
-GROQ_MODEL = "deepseek-r1-distill-llama-70b"
-PEXELS_API_KEY = "" #update with your key
+import os
+
+QLOO_API_KEY = os.getenv("QLOO_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_MODEL = os.getenv("GROQ_MODEL", "deepseek-r1-distill-llama-70b")
+PEXELS_API_KEY = os.getenv("PEXELS_API_KEY")
